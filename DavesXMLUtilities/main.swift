77f92e05d5e3db0762160b03e118791d373799f5
David Christensen <dch@dis-play.dk>
2016-02-26 18:49:09 +0100

    Initial Commit

---
+++ DavesXMLUtilities/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DavesXMLUtilities
+//
+//  Created by David Christensen on 26/02/16.
+//  Copyright © 2016 David Christensen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
